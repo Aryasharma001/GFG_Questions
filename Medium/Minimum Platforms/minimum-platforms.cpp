a1b83c52cc9f6935f1bb7210e5c59b7febb9645c
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-05-31 23:40:33 +0530

    Update minimum-platforms.cpp

--- Medium/Minimum Platforms/minimum-platforms.cpp
+++ Medium/Minimum Platforms/minimum-platforms.cpp
@@ -10,6 +10,7 @@ class Solution{
     public:
     //Function to find the minimum number of platforms required at the
     //railway station such that no train waits.
+    //gfg code
     int findPlatform(int arr[], int dep[], int n)
     {
     	// Your code here
